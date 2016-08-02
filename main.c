850b02cb7f25db84d17036f6574a95286cfc1972
“chen” <chenen_6@hotmail.com>
2016-08-02 23:37:19 +0800

    添加了memset和out数组的定义

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 int main(int arg, char *argv[])
 {
 	int i = 0;
+	char out[1024];
 	
 	LogD("%d\n", arg);
 	for(i = 0; i < arg; i++){
@@ -14,5 +15,7 @@ int main(int arg, char *argv[])
 
 	printf("add a\n");
 	printf("add b\n");
+	
+	memset(out, 0x00, sizeof(out));
 	return 0;
 }
