de8a835c148221cc9e7cb4503bf87a1fda90eb98
“chen” <chenen_6@hotmail.com>
2016-08-02 23:31:42 +0800

    create file main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "libcommon.h"
+#include "log.h"
+
+int main(int arg, char *argv[])
+{
+	int i = 0;
+	
+	LogD("%d\n", arg);
+	for(i = 0; i < arg; i++){
+		LogD("%s\n",argv[i]);
+	}
+
+	return 0;
+}
